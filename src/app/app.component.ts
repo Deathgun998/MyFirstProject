6b5dc3001ab00963672639bb8c5b8097b46afbd6
Deathgun998 <davi.fuma@hotmail.com>
2018-01-29 16:59:37 +0100

    29/01/2018 modifica angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Nuovofile } from './nuovofile';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  items: Nuovofile[] = [];
+  valore: string;
+constructor(){
+  this.title="cypo";
+  this.items.push(new Nuovofile("","",0));
+  this.items.push(new Nuovofile("esatto"));
+  this.items.push(new Nuovofile("maina","gioia"));
+  
+  
+  
+}
+showValue(){
+  alert("valore: "+ this.valore);
+  this.valore="";
+}
+
 }
---
+++ src/app/nuovofile.ts
@@ -0,0 +1,12 @@
+export class Nuovofile{
+    tipo: string;
+    tipoe: string;
+    tipoes: number;
+
+
+    constructor(tipo: string = "esatto", tipoe: string ="sì",tipoes: number = undefined){
+this.tipo = tipo;
+this.tipoe = tipoe
+this.tipoes = tipoes}
+
+}
