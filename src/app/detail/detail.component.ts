446389fb2853e1d4472beea63fda4c4a64e0b259
Deathgun998 <davi.fuma@hotmail.com>
2018-02-21 12:56:00 +0100

    goback

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 import {Nuovofile} from '../nuovofile'
 import { ComunicatorService } from '../comunicator.service';
 import { ListService } from '../list.service';
@@ -10,12 +11,15 @@ import { ListService } from '../list.service';
 })
 export class DetailComponent {
    currentNuovofile : Nuovofile;
-  constructor(private route:ActivatedRoute,private listService:ListService ) {
+  constructor(private route:ActivatedRoute,private listService:ListService,private unacosaacaso: Location ) {
    this.route.params.subscribe(params =>{
      if(params['id'] != '' && params['id'] != null) 
 this.currentNuovofile = this.listService.getNuovofileById(params['id']);
      })
    
+  }
+  torna(){
+    this.unacosaacaso.back();
   }
 }
 
